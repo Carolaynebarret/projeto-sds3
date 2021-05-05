88cd006f5b754dd91989ea805498880152a48796
carolaynebarret <carolaynebarreto@alu.uern.br>
2021-05-04 23:25:01 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
+   <>
+    <NavBar />
     <div >
       <h1 className="text-primary">Olá mundo!</h1>
-    </div>
-  );
+
+      <DataTable />
+        </div>
+        <Footer />
+    </>
+    );
 }
 
 export default App;
