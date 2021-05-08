b1d6d235534a180f2b05240726dea3a8334921eb
carolaynebarret <carolaynebarreto@alu.uern.br>
2021-05-08 19:15:50 -0300

    BarChart integration

--- frontend/src/components/DonoutChart/index.tsx
+++ frontend/src/components/DonoutChart/index.tsx
@@ -1,27 +1,32 @@
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
-type ChartData = {
+type CharData = {
     labels: string[];
     series: number[];
 }
 
 const DonutChart = () => {
 
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    const [chartData, setChartData] = useState<CharData>({ labels: [], series: [] });
 
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            setChartData({ labels: myLabels, series: mySeries });
-        });
+                setChartData({ labels: myLabels, series: mySeries });
+
+
+            });
+
+
     }, []);
 
     const options = {
